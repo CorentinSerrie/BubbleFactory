16a7db26f451eb30e12f5148a853dee7605ccaf3
Corentin <serrie.corentin@gmail.com>
2025-01-26 05:35:13 +0100

    first build and scoring

--- factory.gd
+++ factory.gd
@@ -6,6 +6,7 @@ extends Node2D
 @export var camera: Camera2D
 
 @export var outro_panel: Control
+@export var outro_score: Label
 @export var circle_center: Node2D
 @export var circle_in: Polygon2D
 @export var circle_out: Polygon2D
@@ -31,7 +32,7 @@ func Restart() -> void:
 	bubble = initial_bubble.duplicate()
 	add_child(bubble)
 	move_child(bubble, 2)
-	
+
 
 func OnButtonPressed() -> void:
 	EnterMachine(current_machine)
@@ -72,22 +73,28 @@ func LaunchOutro() -> void:
 	CenterShapeInOutro(bubble_one)
 	CenterShapeInOutro(bubble_two)
 
+	var difference_area = ComputeDifferenceArea()
+	var circle_area = _GetPackedArrayArea(circle_in.polygon)
+
+	var score = (1 - difference_area/circle_area) * 100
+	outro_score.text = ("accuracy: %0.2f%%" % score)
+
 func CenterShapeInOutro(shape: SS2D_Shape) -> void:
 	machines[0]._CenterShape(shape)
 	shape.global_position = circle_center.global_position
 
 func GetShapeRadius(shape: SS2D_Shape) -> float:
 	var total_radius : float = 0
-	
+
 	var tessellated_array: PackedVector2Array = shape.get_point_array().get_tessellated_points()
 
 	for k in range(0, tessellated_array.size()-1):
 		var point_1_k: Vector2 = tessellated_array[tessellated_array.size()-2]
 		if(k > 0):
-			point_1_k = tessellated_array[k-1]			
+			point_1_k = tessellated_array[k-1]
 		var point_k: Vector2 = tessellated_array[k]
 		var point_k_1: Vector2 = tessellated_array[k+1]
-		
+
 		var angle_before = point_1_k.angle_to(point_k)
 		var angle_after = point_k.angle_to(point_k_1)
 		var weight = ((angle_before + angle_after) / 2) / (2 * PI)
@@ -104,17 +111,42 @@ func UpdateCirclePolygons(radius: float):
 	for i in range(0,circle_edge_count):
 		circle_in_array.append(Vector2.RIGHT.rotated(2 * PI * i/circle_edge_count) * radius)
 		circle_out_array.append(Vector2.RIGHT.rotated(2*PI*i/circle_edge_count) * radius)
-	
+
 	circle_out_array.append(Vector2.RIGHT * radius)
 	circle_out_array.append(Vector2.ONE * 5000)
 	circle_out_array.append(Vector2(1,-1) * 5000)
 	circle_out_array.append(Vector2(-1,-1)* 5000)
 	circle_out_array.append(Vector2(-1,1)* 5000)
 	circle_out_array.append(Vector2.ONE * 5000)
-	
+
 	circle_in.polygon = circle_in_array
 	circle_out.polygon = circle_out_array
 
 	circle_in.global_position = circle_center.global_position
 	circle_out.global_position = circle_center.global_position
 	pass
+
+
+func ComputeDifferenceArea() -> float:
+	var tessellated_bubble = bubble.get_point_array().get_tessellated_points()
+
+	var total_area:float = 0
+
+	var difference: Array[PackedVector2Array] = Geometry2D.clip_polygons(tessellated_bubble, circle_in.polygon)
+	for polygon_array in difference:
+		total_area += _GetPackedArrayArea(polygon_array)
+
+	difference = Geometry2D.clip_polygons(circle_in.polygon, tessellated_bubble)
+	for polygon_array in difference:
+		total_area += _GetPackedArrayArea(polygon_array)
+
+	return total_area
+
+func _GetPackedArrayArea(packed_array: PackedVector2Array) -> float:
+	var area: float = 0
+
+	for k in range(0, packed_array.size()-1):
+		var point_k: Vector2 = packed_array[k]
+		var point_k_1: Vector2 = packed_array[k+1]
+		area += point_k.x * point_k_1.y - point_k_1.x * point_k.y
+	return area/2
